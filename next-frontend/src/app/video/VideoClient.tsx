b40ab88d684618710ac4327b73127dc338f8f72f
WhiteSnake <whitesnake35711@gmail.com>
2026-07-05 17:20:37 +0530

    tried fixing some bugs

---
+++ next-frontend/src/app/video/VideoClient.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+import Comments from "@/components/comments";
+import RecommendedVideoList from "@/components/recommended";
+import VideoSection from "@/components/video";
+import CommentProvider from "@/context/comment_provider";
+import { useVideo } from "@/context/video_provider";
+import { VideoDetailstype, VideoImages } from "@/types/video.types";
+
+export default function Video() {
+  const { getVideoById, getVideoFiles } = useVideo();
+
+  const searchParams = useSearchParams();
+  const videoId = searchParams.get("videoId");
+
+  const [video, setVideo] = useState<VideoDetailstype | null>(null);
+
+  useEffect(() => {
+    if (!videoId) return;
+
+    const fetchVideo = async () => {
+      try {
+        const response = await getVideoById(videoId);
+        if (!response) return;
+
+        const videoFiles: VideoImages[] | null = await getVideoFiles([videoId]);
+        if (!videoFiles?.length) return;
+
+        setVideo({
+          ...response,
+          thumbnail: videoFiles[0].thumbnail,
+          videoUrl: videoFiles[0].video,
+          guildAvatar: videoFiles[0].avatar,
+        });
+      } catch (error) {
+        console.error("Error fetching video:", error);
+      }
+    };
+
+    fetchVideo();
+  }, [videoId, getVideoById, getVideoFiles]);
+
+  if (!videoId) {
+    return <div>Invalid video.</div>;
+  }
+
+  if (!video) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="relative grid grid-cols-12 p-4 gap-2">
+      <div className="col-span-8">
+        <VideoSection video={video} />
+        <CommentProvider>
+          <Comments videoId={video.id} />
+        </CommentProvider>
+      </div>
+
+      <div className="col-span-4">
+        <h1 className="text-2xl font-bold">Recommended Videos</h1>
+        <hr className="border-t border-red-700 my-4" />
+        <RecommendedVideoList videoId={video.id} />
+      </div>
+    </div>
+  );
+}
