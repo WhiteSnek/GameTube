d9b336aa21e4248963b81822b0316364cc7c04be
WhiteSnake <whitesnake35711@gmail.com>
2026-07-06 21:00:16 +0530

    removed db url

--- backend/src/config/database.go
+++ backend/src/config/database.go
@@ -2,15 +2,11 @@ package config
 
 import (
 	"log"
-	"os"
 	"github.com/WhiteSnek/GameTube/prisma/db"
 )
 
 func ConnectDB() (*db.PrismaClient, error) {
-	databaseURL := os.Getenv("DATABASE_URL")
-	dbClient := db.NewClient(
-		db.WithDatasourceURL(databaseURL),
-	)
+	dbClient := db.NewClient()
 	if err := dbClient.Prisma.Connect(); err != nil {
 		return nil, err
 	}
