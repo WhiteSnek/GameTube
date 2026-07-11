f0f63591aec108a51362731dc265bbb438e0f769
WhiteSnake <whitesnake35711@gmail.com>
2026-07-11 13:52:00 +0530

    bug fixes

--- next-frontend/src/assets/index.ts
+++ next-frontend/src/assets/index.ts
@@ -1,9 +1,8 @@
 import LoginImage from './login.png'
 import SignupImage from './signup.png'
-const CLOUDFRONT_URL = process.env.NEXT_PUBLIC_CLOUDFRONT_URL
-const DefaultAvatar = `${CLOUDFRONT_URL}/images/defaults/profile/avatar/default-avatar.jpg`
-const DefaultGuildavatar = `${CLOUDFRONT_URL}/images/defaults/guild/avatar/default-guild-avatar.jpg`
-const DefaultGuildCover = `${CLOUDFRONT_URL}/images/defaults/guild/coverImage/default-guild-cover.jpg`
+const DefaultAvatar = `/images/defaults/profile/avatar/default-avatar.jpg`
+const DefaultGuildavatar = `/images/defaults/guild/avatar/default-guild-avatar.jpg`
+const DefaultGuildCover = `/images/defaults/guild/coverImage/default-guild-cover.jpg`
 export {
     LoginImage,
     SignupImage,
